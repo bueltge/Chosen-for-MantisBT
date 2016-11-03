622f312d8cbf4d8c7fdad980db740635a2e9144b
bueltge <frank@bueltge.de>
2016-11-03 20:15:00 +0100

    release new version

--- chosen/chosen.php
+++ chosen/chosen.php
@@ -1,19 +1,4 @@
 <?php
-# Copyright (C) 2012-2016 Frank Bültge
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
-
 class ChosenPlugin extends MantisPlugin {
 
 	/**
@@ -25,7 +10,7 @@ class ChosenPlugin extends MantisPlugin {
 		
 		$this->name        = plugin_lang_get( 'title' );
 		$this->description = plugin_lang_get( 'description' );
-		$this->version     = '1.1.0';
+		$this->version     = '1.1.1';
 		$this->requires    = array(
 			'MantisCore' => '1.2.0, 1.3.0',
 		);
